7ff231000b5aab3e0b0e9faac8eb2259fee1c0d1
jkpubsrc <pubsrc@binary-overflow.de>
2018-10-06 18:46:25 +0200

    Commit.

--- src/jk_utils/fsutils.py
+++ src/jk_utils/fsutils.py
@@ -4,14 +4,18 @@ import os
 import collections
 
 
-DiskSpaceStats = collections.namedtuple("DiskSpaceStats", [ "total", "used", "freeSystem", "freeUser" ])
+DiskSpaceStats = collections.namedtuple("DiskSpaceStats", [ "bytesTotal", "bytesUsed", "bytesFreeSystem", "bytesFreeUser", "inodesTotal", "inodesUsed", "inodesFree" ])
 
 def getFileSystemStats(mountPointPath):
 	statvfs = os.statvfs(mountPointPath)
-	totalBytes = statvfs.f_frsize * statvfs.f_blocks		# Size of filesystem in bytes
-	systemFreeBytes = statvfs.f_frsize * statvfs.f_bfree	# Actual number of free bytes
-	userFreeBytes = statvfs.f_frsize * statvfs.f_bavail		# Number of free bytes that ordinary users are allowed to use (excl. reserved space)
-	return DiskSpaceStats(totalBytes, totalBytes - systemFreeBytes, systemFreeBytes, userFreeBytes)
+	bytesTotal = statvfs.f_frsize * statvfs.f_blocks		# Size of filesystem in bytes
+	bytesFreeSystem = statvfs.f_frsize * statvfs.f_bfree	# Actual number of free bytes
+	bytesFreeUser = statvfs.f_frsize * statvfs.f_bavail		# Number of free bytes that ordinary users are allowed to use (excl. reserved space)
+	bytesUsed = bytesTotal - bytesFreeSystem
+	inodesTotal = statvfs.f_files    					    # inodes
+	inodesFree = statvfs.f_ffree	   						# free inodes
+	inodesUsed = inodesTotal - inodesFree
+	return DiskSpaceStats(bytesTotal, bytesUsed, bytesFreeSystem, bytesFreeUser, inodesTotal, inodesUsed, inodesFree)
 #
 
 
